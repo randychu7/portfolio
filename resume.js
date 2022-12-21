c7d8872759d316566d06aeac035d80d406de7dd9
Randy Chu <randychu7@gmail.com>
2022-12-21 13:00:36 -0600

    madechanges

--- resume.js
+++ resume.js
@@ -1,11 +1,3 @@
-const parallaxElements = document.querySelectorAll('.parallax-layer');
-
-window.addEventListener('scroll', () => {
-  let scrollTop = window.pageYOffset;
-
-  parallaxElements.forEach(parallaxElement => {
-    let speed = parallaxElement.getAttribute('data-speed');
-
-    parallaxElement.style.transform = `translateY(${scrollTop * speed}px)`;
-  });
-});
+let controller = new scrollMagic.Controller();
+let timeline = new TimelineMax();
+timeline
